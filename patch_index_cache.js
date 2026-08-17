1a1434c7c6af4fefac7bb0d9141a0478f4c36de6
lousyqq <lousyqqmm2351@gmail.com>
2026-08-17 23:35:57 +0800

    20260817_v2

--- patch_index_cache.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('wwwroot/index.html', 'utf8');
-
-// Replace any existing app.css or app.css?v=... with a new timestamp
-html = html.replace(/href="app\.css(\?v=\d+)?"/, 'href="app.css?v=' + Date.now() + '"');
-html = html.replace(/src="app\.js(\?v=\d+)?"/, 'src="app.js?v=' + Date.now() + '"');
-
-fs.writeFileSync('wwwroot/index.html', html);
-console.log('Cache busting applied to index.html');
--- patch_program_mock.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('Program.cs', 'utf8');
-
-code = code.replace(/history = new List<object>\(\)/g, 'history = ""');
-
-fs.writeFileSync('Program.cs', code);
-console.log('Fixed CS0029 errors.');
